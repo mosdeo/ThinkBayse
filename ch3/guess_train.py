f5dce1acf0be51f1567b097f143f2dfe28bf2aeb
Lin, Kao-Yuan <mosdeo@gmail.com>
2021-07-06 17:07:48 +0800

    加入證明多次觀測的結果，對上限不敏感。

--- ch3/guess_train.py
+++ ch3/guess_train.py
@@ -1,17 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 num_in_train = list(range(1, 1001))
-prior_rate = [1 for n in num_in_train]
+hypo = [1 for n in num_in_train]
 got_num = 60
-likelihood = [1/s if got_num <= s else 0 for s in num_in_train]
-priori_x_likelihood = np.array(likelihood)*np.array(prior_rate)
-probability =  [l/sum(priori_x_likelihood) for l in priori_x_likelihood]
-print('Probability:')
+likelihood = np.array([1/s if got_num <= s else 0 for s in num_in_train]) 
+hypo_x_likelihood = np.array(hypo)*np.array(likelihood)
+probability = [l/sum(hypo_x_likelihood) for l in hypo_x_likelihood]
+
+# print('Probability:')
 avg = 0
-# for n, p  in zip(num_in_train, probability):
-#     print('N:{:2}, P(N):{:0.3}'.format(n, p))
-#     avg += n*p
-print("avg:{}".format(avg))
+for n, p  in zip(num_in_train, probability):
+    # print('N:{:2}, P(N):{:0.3}'.format(n, p))
+    avg += n*p
+print("posteriori avg:{}".format(avg))
 
 # Visualization
 plt.plot(probability)
---
+++ ch3/guess_train_mulit_data.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 三種上限。證明多次觀測的結果，對上限不敏感
+for uppper in [500, 1000, 2000]:
+    num_in_train = list(range(1, uppper+1))
+    hypo = [1 for n in num_in_train]
+
+    # 將多次觀測到的數據，分別計算出 likelihood，然後求 dot product
+    likelihood = np.ones(shape=(len(num_in_train)))
+    for got_num in [60 ,30 ,90]:
+        likelihood *= np.array([1/s if got_num <= s else 0 for s in num_in_train]) 
+
+    hypo_x_likelihood = np.array(hypo)*np.array(likelihood)
+    probability = [l/sum(hypo_x_likelihood) for l in hypo_x_likelihood]
+
+    # print('Probability:')
+    avg = 0
+    for n, p  in zip(num_in_train, probability):
+        # print('N:{:2}, P(N):{:0.3}'.format(n, p))
+        avg += n*p
+    print("posteriori avg:{}".format(avg))
+
+    # Visualization
+    plt.plot(probability)
+    plt.show()
