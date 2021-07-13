040293ea01cfbd043f8f4e5a777544eb5970769c
Lin, Kao-Yuan <mosdeo@gmail.com>
2021-07-13 09:52:56 +0800

    有背景資料優化的先驗

---
+++ ch3/guess_train_priori_with_powerlaw.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+uppers = [500, 1000, 2000] # 三種上限。證明多次觀測的結果，對上限不敏感
+observes = [60 ,30 ,90] # 多次觀測到的數據
+
+def powerlaw(x):
+    return 1/x
+
+for uppper in uppers:
+    num_in_train = list(range(1, uppper+1))
+    hypo = list(map(powerlaw, num_in_train))
+
+    # 將多次觀測到的數據，分別計算出 likelihood，然後求 dot product
+    likelihood = np.ones(shape=(len(num_in_train)))
+    for got_num in observes:
+        likelihood *= np.array([1/s if got_num <= s else 0 for s in num_in_train]) 
+    
+
+    hypo_x_likelihood = np.array(hypo)*np.array(likelihood)
+    probability = [l/sum(hypo_x_likelihood) for l in hypo_x_likelihood]
+
+    # print('Probability:')
+    avg = 0
+    for n, p  in zip(num_in_train, probability):
+        # print('N:{:2}, P(N):{:0.3}'.format(n, p))
+        avg += n*p
+    print("posteriori avg:{}".format(avg))
+
+    # Visualization
+    plt.title("Upper = {}".format(uppper))
+    plt.xlabel("Num of train")
+    plt.ylabel("Probability")
+    plt.plot(probability)
+    plt.show()
