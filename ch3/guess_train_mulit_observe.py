b127ae56a7761bff6c528de8b56c4877fe69af46
Lin, Kao-Yuan <mosdeo@gmail.com>
2021-07-13 09:14:01 +0800

    把內容寫得更清楚

--- ch3/guess_train_mulit_observe.py
+++ ch3/guess_train_mulit_observe.py
@@ -1,14 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 三種上限。證明多次觀測的結果，對上限不敏感
-for uppper in [500, 1000, 2000]:
+uppers = [500, 1000, 2000] # 三種上限。證明多次觀測的結果，對上限不敏感
+observes = [60 ,30 ,90] # 將多次觀測到的數據
+
+for uppper in uppers:
     num_in_train = list(range(1, uppper+1))
     hypo = [1 for n in num_in_train]
 
     # 將多次觀測到的數據，分別計算出 likelihood，然後求 dot product
     likelihood = np.ones(shape=(len(num_in_train)))
-    for got_num in [60 ,30 ,90]:
+    for got_num in observes:
         likelihood *= np.array([1/s if got_num <= s else 0 for s in num_in_train]) 
 
     hypo_x_likelihood = np.array(hypo)*np.array(likelihood)
